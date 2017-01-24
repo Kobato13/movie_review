8c8eb55cd3769280d3bc9c1ffad4784e3aa5f46f
Kobato13 <johncarlomartinez13@gmail.com>
2017-01-24 05:10:55 -0800

    2nd commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :reviews
+  devise_for :users
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end
+  root 'movies#index'
+end
